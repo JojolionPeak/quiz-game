8e21ca1fd6a070a3671f931b7055c7f3e5207772
Jonathan <JohnAnBer@gmail.com>
2025-12-24 10:36:52 -0500

    fixed up leaderboard display

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,22 +1,23 @@
-const fs = require('fs');
+const fs = require("fs");
 
 class Leaderboard {
-
   scores = this.loadScores();
 
-  saveScores(){
+  saveScores() {
     try {
-      fs.writeFileSync('./data.json', JSON.stringify(this.scores, null));
+      fs.writeFileSync("./data.json", JSON.stringify(this.scores, null));
     } catch (error) {
-      console.log(`Could not load leaderboard. Error message: ${error.message}`);
-    };
+      console.log(
+        `Could not load leaderboard. Error message: ${error.message}`
+      );
+    }
   }
 
-  addScore(id, score){
+  addScore(id, score) {
     this.scores.push({
-      "name": id,
-      "score": score,
-      "date": new Date()
+      name: id,
+      score: score + "%",
+      date: new Date(),
     });
 
     this.scores.sort((a, b) => b.score - a.score);
@@ -25,31 +26,31 @@ class Leaderboard {
 
   loadScores() {
     try {
-      if (fs.existsSync('./data.json')) {
-        const rankings = fs.readFileSync('./data.json', 'utf8');
+      if (fs.existsSync("./data.json")) {
+        const rankings = fs.readFileSync("./data.json", "utf8");
         return JSON.parse(rankings);
-      };
+      }
     } catch (error) {
-      console.error('Error loading leaderboard:', error.message);
-    };
+      console.error("Error loading leaderboard:", error.message);
+    }
     return [];
-  };
+  }
 
-  get topScores(){
-   return this.scores.slice(0, 5);
-  };
+  get topScores() {
+    return this.scores.slice(0, 5);
+  }
 
-  showBoard(){
-    console.log('\n✦✦✦ SCOREBOARD ✦✦✦');
-    if (this.scores.length === 0){
-      console.log('No saved scores yet. Share your score after playing!');
+  showBoard() {
+    console.log("\n✦✦✦ SCOREBOARD ✦✦✦");
+    if (this.scores.length === 0) {
+      console.log("No saved scores yet. Share your score after playing!");
       return;
-    };
+    }
     this.scores.forEach((entry, index) => {
       console.log(`${index + 1}. ${entry.name}: ${entry.score}`);
     });
-    console.log('✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦\n');
-  };
-};
+    console.log("✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦\n");
+  }
+}
 
 module.exports = { Leaderboard };
